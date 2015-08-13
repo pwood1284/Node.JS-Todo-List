8d42bf0d769c094994e6f3376f49d2e9716ee1ab
pwood1284 <pwood1284@gmail.com>
2015-08-13 15:31:07 -0400

    Added controllers

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,8 @@ module.exports = router;
 router.post('/api/v1/todos', function(req, res) {
   var results = [];
   // Grab data from http request
-  var data = {text: req.body.text, complete: false};
+  var data = {text: req.body.text,
+              complete: false};
 
   // Get a Postgres client from the connection pool
   pg.connect(connectionString, function(err, client, done) {
@@ -60,6 +61,78 @@ router.get('/api/v1/todos', function(req, res) {
       client.end();
       return res.json(results);
     });
+    // Handle Errors
+    if(err) {
+      console.log(err);
+    }
+  });
+});
+
+/* Put (update) todo item */
+router.put('/api/v1/todos/:todo_id', function(req, res) {
+  var results = [];
+
+  // Grab data from the URL parameters
+  var id = req.params.todo_id;
+
+  // Grab data from the http request
+  var data = {text: req.body.text,
+              complete: req.body.complete};
+
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, function(err, client, done) {
+
+    // SQL Query > Update Data
+    client.query("UPDATE items SET text=($1), complete=($2) WHERE id=($3)", [ data.text, data.complete, id ]);
+
+    // SQL Query < Select Data
+    var query = client.query("SELECT * FROM items ORDER BY id ASC");
+
+    // Stream results back one row at a time
+    query.on('row', function(row) {
+      results.push(row);
+    });
+
+    // After all data is returned, close connection and return results
+    query.on('end', function() {
+      client.end();
+      return res.json(results);
+    });
+
+    //Handle Errors
+    if(err) {
+      console.log(err);
+    }
+  });
+});
+
+/* Delete todo item */
+router.delete('/api/v1/todos/:todo_id', function(req, res) {
+  var results = [];
+
+  // Grab data from the URL parameters
+  var id = req.params.todo_id;
+
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, function(err, client, done) {
+
+    // SQL Query > Delete Data
+    client.query("DELETE FROM items WHERE id=($1)", [id]);
+
+    // SQL Query > Select Data
+    var query = client.query("SELECT * FROM items ORDER BY id ASC");
+
+    // Stream results back on row at a time
+    query.on('row', function(row) {
+      results.push(row);
+    });
+
+    // After all data is returned, close connection and return results
+    query.on('end', function() {
+      client.end();
+      return res.json(results);
+    });
+
     // Handle Errors
     if(err) {
       console.log(err);
